fe132fedf5b06bbec752935557b696a246d2b57a
VIJAY BAJRACHARYA <u1198617@lab1-11.eng.utah.edu>
2020-03-14 14:26:13 -0600

    asifenc.c placeholder code

--- libavcodec/asifenc.c
+++ libavcodec/asifenc.c
@@ -3,13 +3,40 @@
 
 static av_cold int asif_encode_init(AVCodecContext *avctx)
 {
-  avctx->codec_id = AV_CODEC_ID_ASIF;
+  avctx->frame_size = 0; 
+  avctx->bits_per_coded_sample = av_get_bits_per_sample(avctx->codec->id);
+  avctx->block_align           = avctx->channels * avctx->bits_per_coded_sample / 8;
+  avctx->bit_rate              = avctx->block_align * 8LL * avctx->sample_rate;
+
   return 0;
 }
+
+#define ENCODE(type, endian, src, dst, n, shift, offset)                \
+    samples_ ## type = (const type *) src;                              \
+    for (; n > 0; n--) {                                                \
+        register type v = (*samples_ ## type++ >> shift) + offset;      \
+        bytestream_put_ ## endian(&dst, v);                             \
+    }
 
 static int asif_encode_frame(AVCodecContext *avctx, AVPacket *avpkt,
 			     const AVFrame *frame, int *got_packet_ptr)
 {
+  /*
+   * Number of samples: n
+   * Source pointer: src
+   * Destination pointer: dst
+   */
+  
+  bytestream_put_byte('a');
+  bytestream_put_byte('s');
+  bytestream_put_byte('i');
+  bytestream_put_byte('f');
+  bytestream_put_le32(/*sample_rate*/);
+  bytestream_put_le16(/*number_of_channels*/);
+  bytestream_put_le32(/*number_of_samples_per_channel*/);
+  bytestream_put_uint8_t(/*Channel0_data*/);
+  bytestream_put_uint8_t(/*Channel1_data*/);
+
   return 0;
 }
 
